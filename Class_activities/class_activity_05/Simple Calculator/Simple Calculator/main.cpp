07d2d89ef85bb4846bc96ed7ff45e7c613575f34
Nan Sayng <pppont4400@gmail.com>
2025-01-31 17:46:47 -0800

    Update

--- Class_activities/class_activity_05/Simple Calculator/Simple Calculator/main.cpp
+++ Class_activities/class_activity_05/Simple Calculator/Simple Calculator/main.cpp
@@ -1,36 +1,36 @@
 #include <iostream>
+#include <iomanip>
 using namespace std;
 
 int main(){
-    char opt;
+    char op;
     cout << "Choose the operation (+, -, *, /): ";
-    cin >> opt;
-    int num1, num2;
+    cin >> op;
+    double num1, num2;
     cout << "Enter the first number: ";
     cin >> num1;
     cout << "Enter the second number: ";
     cin >> num2;
     
-    switch (opt) {
+    switch (op) {
         case '+':
-            cout << num1 << " + " << num2 << " = " << num1 + num2 << endl;
+            cout << num1 << " " << op << " " << num2 << " = " << num1 + num2 << endl;
             break;
         case '-':
-            cout << num1 << " - " << num2 << " = " << num1 - num2 << endl;
+            cout << num1 << " " << op << " " << num2 << " = " << num1 - num2 << endl;
             break;
         case '*':
-            cout << num1 << " * " << num2 << " = " << num1 * num2 << endl;
+            cout << num1 << " " << op << " " << num2 << " = " << num1 * num2 << endl;
             break;
         case '/':
             if(num2 == 0)
-                cout << "Error! Cannot be divided by zero!";
+                cout << "Error! Cannot be divided by zero!\n";
             else
-                cout << num1 << " / " << num2 << " = " << num1 / num2 << endl;
+                cout << num1 << " " << op << " " << num2 << " = " << num1 / num2 << endl;
             break;
         default:
-            cout << "Invalid operation!\n";
+            cout << "Invalid operation!" << endl;
             break;
     }
-    
     return 0;
 }
