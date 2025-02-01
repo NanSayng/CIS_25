31f803a92f53f7f396f03e13ee83028797b9a191
Nan Sayng <pppont4400@gmail.com>
2025-01-31 23:43:02 -0800

    Initial commit

--- Class_activities/class_activity_07/Leap Year Checker/Leap Year Checker/main.cpp
+++ Class_activities/class_activity_07/Leap Year Checker/Leap Year Checker/main.cpp
@@ -7,7 +7,7 @@ int main(){
     cin >> year;
     bool isLeapYear;
     
-    if (year % 4 == 0 || year % 100 == 0 && year % 400 == 0)
+    if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0)
         isLeapYear = true;
     else
         isLeapYear = false;
