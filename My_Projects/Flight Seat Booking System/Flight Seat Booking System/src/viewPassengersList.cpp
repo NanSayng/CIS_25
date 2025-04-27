852e42f0a9f1dea720a3c70594212ba47b4724a0
Nan Sayng <pppont4400@gmail.com>
2025-04-26 20:34:26 -0700

    Updated viewPassengersList.cpp

--- My_Projects/Flight Seat Booking System/Flight Seat Booking System/src/viewPassengersList.cpp
+++ My_Projects/Flight Seat Booking System/Flight Seat Booking System/src/viewPassengersList.cpp
@@ -2,10 +2,17 @@
 
 void viewPassengersList(){
     int num = 1;
-    cout << "\nPassenger List: \n";
+    cout << "\nPassengers list: \n";
     if(!passengers.empty()){
         for(auto& passengerPair: passengers){
-            cout << num << ". " << passengerPair.second.name << " | Age: " << passengerPair.second.age << " | Phone: " << passengerPair.second.contact << " | Passport: " << passengerPair.second.passportNumber << " Seat: " << passengerPair.second.seatNumber << endl;
+            cout << num << ". " << passengerPair.second.name << " | Age: " << passengerPair.second.age << " | Phone: " << passengerPair.second.contact << " | Passport: " << passengerPair.second.passportNumber << " | Seat: " << passengerPair.second.seatNumber ;
+            if(passengerPair.second.seatClass == SeatClass::First){
+                cout << " (First class)\n";
+            }else if(passengerPair.second.seatClass == SeatClass::Business){
+                cout << " (Business class)\n";
+            }else{
+                cout << " (Economy class)\n";
+            }
             num++;
         }
     }else{
