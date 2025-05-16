f406de488501ca64e3039d49eec246dc15ebce33
Nan Sayng <pppont4400@gmail.com>
2025-05-16 11:15:23 -0700

    Added Entry class as base class for Meal/Workout class

--- My_Projects/Fitness_Tracker(OOP ver)/Fitness_Tracker(OOP ver)/include/entry.h
+++ My_Projects/Fitness_Tracker(OOP ver)/Fitness_Tracker(OOP ver)/include/entry.h
@@ -4,4 +4,22 @@
 //
 //  Created by Nan Sayng on 5/15/25.
 //
+#pragma once
+#include <string>
+using namespace std;
 
+class Entry{
+protected:
+    string name;
+    double calories;
+public:
+    Entry();
+    Entry(const string& name, const double& calories);
+    // set the values
+    void setName(const string& name);
+    void setCalories(const double& calories);
+    // get the values
+    virtual string getName() const;
+    virtual double getCalories() const;
+    virtual ~Entry() {}
+};
--- My_Projects/Fitness_Tracker(OOP ver)/Fitness_Tracker(OOP ver)/src/entry.cpp
+++ My_Projects/Fitness_Tracker(OOP ver)/Fitness_Tracker(OOP ver)/src/entry.cpp
@@ -4,4 +4,34 @@
 //
 //  Created by Nan Sayng on 5/15/25.
 //
+#include "../include/entry.h"
+
+Entry::Entry() : name("unknown"), calories(0.0) {}
+
+Entry::Entry(const string& name, const double& calories){
+    setName(name);
+    setCalories(calories);
+}
+
+void Entry::setName(const string& name){
+    this->name = name;
+}
+
+void Entry::setCalories(const double& calories){
+    if(calories <= 0){
+        throw std::invalid_argument("The calories can't be zero or negative.\n");
+    }
+    this->calories = calories;
+}
+
+string Entry::getName() const{
+    return name;
+}
+
+double Entry::getCalories() const{
+    return calories;
+}
+
+
+
 
