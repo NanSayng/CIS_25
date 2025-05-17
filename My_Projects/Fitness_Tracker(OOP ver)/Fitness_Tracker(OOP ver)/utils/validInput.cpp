5e9b3e9b94ac013d69fd0de9ffa957b13e2cf37a
Nan Sayng <pppont4400@gmail.com>
2025-05-16 23:09:24 -0700

    Updated validations fucntions to check user inputs

--- My_Projects/Fitness_Tracker(OOP ver)/Fitness_Tracker(OOP ver)/utils/validInput.cpp
+++ My_Projects/Fitness_Tracker(OOP ver)/Fitness_Tracker(OOP ver)/utils/validInput.cpp
@@ -10,16 +10,37 @@
 
 bool validName(const string& name){
     regex reg(R"(^[A-Za-z ]*$)");
-    if(regex_match(name, reg))
-        return true;
-    return false;
+    return (regex_match(name, reg)) ? true : false;
 }
 
 bool validGender(const string& gender){
     regex reg(R"(^[mMfF]$)");
-    if(regex_match(gender, reg)){
-        return true;
-    }
-    return false;
+    return (regex_match(gender, reg)) ? true : false;
 }
+
+bool validCalories(const string& calories){
+    regex reg(R"(^([1-9][0-9]*|0)(\.[0-9]+)?$)");
+    return (regex_match(calories, reg)) ? true : false;
+}
+
+bool validHeight(const string& height){
+    regex reg(R"(^([1-9][0-9]*|0)(\.[0-9]+)?$)");
+    return (regex_match(height, reg)) ? true : false;
+}
+
+bool validWeight(const string& weight){
+    regex reg (R"(^([1-9][0-9]*|0)(\.[0-9]+)?$)");
+    return (regex_match(weight, reg)) ? true : false;
+}
+
+bool validMenuOption(const string& option){
+    regex reg(R"(^[1-8]$)");
+    return (regex_match(option, reg)) ? true : false;
+}
+
+bool validYesOrNo(const string& input){
+    regex reg(R"(^[yYnN]$)");
+    return (regex_match(input, reg)) ? true : false;
+}
+
 
