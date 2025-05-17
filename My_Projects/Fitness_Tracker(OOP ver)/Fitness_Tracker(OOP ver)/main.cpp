a122a2196306715c5cb8e0fc2b14c4ecc11b9b83
Nan Sayng <pppont4400@gmail.com>
2025-05-16 23:11:21 -0700

    Updated main.cpp to run the program

--- My_Projects/Fitness_Tracker(OOP ver)/Fitness_Tracker(OOP ver)/main.cpp
+++ My_Projects/Fitness_Tracker(OOP ver)/Fitness_Tracker(OOP ver)/main.cpp
@@ -1,1 +1,12 @@
+#include <iostream>
+#include "./include/fitnessTracker.h"
+using namespace std;
 
+int main(){
+    // title
+    cout << setfill(' ') << setw(50) << "Welcome to Nan's Health and Fitness Tracker\n";
+    cout << setfill('-') << setw(57) << '-' << endl;
+    FitnessTracker fitnessTracker;
+    fitnessTracker.runProgram();
+    return 0;
+}
