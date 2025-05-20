04d04610fd9fca27c5892b99cf165349e1d917b3
Nan Sayng <pppont4400@gmail.com>
2025-05-20 14:23:00 -0700

    Adds comments and steps

--- My_Projects/mygrep/mygrep/grep.cpp
+++ My_Projects/mygrep/mygrep/grep.cpp
@@ -8,38 +8,41 @@
 #include "grep.hpp"
 using namespace std;
 
+// converts string to lower case - used for caseInsenstive case
 string toLowerStr(const string& str){
     string lowerStr  = str;
     for(char& c : lowerStr){
-        c = tolower(c);
+        c = tolower(c); // convert each character to lower case
     }
-    return lowerStr;
+    return lowerStr; // return converted lower-cased string
 }
 
+// Checks if the given keyword exists in the line (case-sensitive or case-insensitive)
 bool matchString(const std::string& line, const std::string& givenStr, bool caseInsensitive){
     if(caseInsensitive){
         string lowerLine = toLowerStr(line);
         string lowerStr = toLowerStr(givenStr);
-        return (lowerLine.find(lowerStr) != string::npos);
+        return (lowerLine.find(lowerStr) != string::npos); // check if keyword is in the line (caseInsensitive)
     }
-    return (line.find(givenStr) != string::npos);
+    return (line.find(givenStr) != string::npos);  // check if keyword is in the line (caseSensitive)
 }
 
+// Reads the text file line-by-line, checks for matches, and prints matching lines
 void readTxtFile(const string& filename, const string& keyword, bool caseInsensitive, bool showLineNum){
     ifstream file(filename); // open file to read
-    if(!file.is_open()){
+    if(!file.is_open()){ // check if file is opened successfully
         cout << "Error opening the file.\n";
         return;
     }
     string line;
-    int countLine = 0;
+    int countLine = 0; // to count the line number
     while(getline(file, line)){
         countLine++;
-        if(matchString(line, keyword, caseInsensitive)){
+        if(matchString(line, keyword, caseInsensitive)){ // if each line contain keyword
             if(showLineNum){
-                cout << "Line " << countLine << ": " << line << endl;
+                cout << "Line " << countLine << ": " << line << endl; // print with line number
             }else{
-                cout << line << endl;
+                cout << line << endl;  // print without line number
             }
         }
     }
--- My_Projects/mygrep/mygrep/main.cpp
+++ My_Projects/mygrep/mygrep/main.cpp
@@ -5,21 +5,21 @@
 using namespace std;
 
 int main(int argc, const char * argv[]) {
-    string filename, keyword;
+    string filename, keyword; // variables to hold user inputs
     
-    bool caseInsensitive = false;
+    bool caseInsensitive = false; // flags for command-line
     bool showLineNum = false;
     
     if(argc < 3 || argc > 5){
-        cout << "Usage: ./mygrep [-i] [-n] <keyword> <filename>\n";
+        cout << "Usage: ./mygrep [-i] [-n] <keyword> <filename>\n"; // output error msg if user num of arguments if too many or too few
         return 1;
     }else {
-        if(argc == 5){
+        if(argc == 5){  // case when provided both -i and -n
             keyword = argv[3];
             filename = argv[4];
             string flag1 = string(argv[1]);
             string flag2 = string(argv[2]);
-            if(flag1 == "-i" && flag2 == "-n" || flag1 == "-n" && flag2 == "-i"){
+            if(flag1 == "-i" && flag2 == "-n" || flag1 == "-n" && flag2 == "-i"){ // check if the flags are valid
                 caseInsensitive = true;
                 showLineNum = true;
             }else {
@@ -27,7 +27,7 @@ int main(int argc, const char * argv[]) {
                 return 1;
             }
             readTxtFile(filename, keyword, caseInsensitive, showLineNum);
-        }else if(argc == 4){
+        }else if(argc == 4){  // case when provided with either -i or -n
             keyword = argv[2];
             filename = argv[3];
             if(string(argv[1]) == "-i"){
@@ -36,7 +36,7 @@ int main(int argc, const char * argv[]) {
                 showLineNum = true;
             }
             readTxtFile(filename, keyword, caseInsensitive, showLineNum);
-        }else {
+        }else { // case when provided none of the flags
             keyword = argv[1];
             filename = argv[2];
             readTxtFile(filename, keyword, caseInsensitive, showLineNum);
